48dbceacae506248fc2196da789ecf9f5a69d32a
James Hughes <jamhughes@deloitte.co.uk>
2022-01-13 20:02:20 +0000

    testing keyboard

--- src/App.js
+++ src/App.js
@@ -39,6 +39,22 @@ export function App() {
     }
   }
 
+  const findBestHistoricalPlacement = (value) => {
+    let best = ""
+    for (let rowIndex = 0; rowIndex < history.length; rowIndex++) {
+      const row = history[rowIndex];
+      for(let index = 0; index < row.length; index++) {
+        if(row[index] === value) {
+          best = position(value, index)
+          if(best === 'inplace' || best === 'missing') {
+            return best;
+          }
+        }
+      }
+    }
+    return best
+  }
+
   const handler = ({key}) => {
     if(!broken) {
       if (!isNaN(key)) {
@@ -67,24 +83,39 @@ export function App() {
   }, [code, current, broken, history])
 
   return <div className="app">
-    <h1>Coddle</h1>
-    <h2>Code Breaker</h2>
-    {
-      history.map((row, rowIndex) => {
-        return <div key={`row-${rowIndex}`}>
-          {row.map((cell, cellIndex) => {
-            return <span key={`row-${rowIndex}-${cellIndex}`} className={`cell ${position(cell, cellIndex)}`}>{cell}</span>
-          })}
-        </div>
-      })
-    }
     <div>
-    { !broken &&
-        [0,1,2,3].map(index => {
-          return <span key={index} className="cell">{current[index] !== undefined ? current[index] : '*'}</span>
+      <h1>Coddle</h1>
+      <h2>Code Breaker</h2>
+      {
+        history.map((row, rowIndex) => {
+          return <div key={`row-${rowIndex}`}>
+            {row.map((cell, cellIndex) => {
+              return <span key={`row-${rowIndex}-${cellIndex}`} className={`cell ${position(cell, cellIndex)}`}>{cell}</span>
+            })}
+          </div>
         })
-    }
-      { broken && <h3>Code broken in {history.length} attempts</h3> }
+      }
+      <div>
+        { !broken &&
+            [0,1,2,3].map(index => {
+              return <span key={index} className="cell">{current[index] !== undefined ? current[index] : '*'}</span>
+            })
+        }
+        {/*{*/}
+        {/*  !broken && <span>({current.reduce((partial_sum, a) => partial_sum + a, 0)} +"/" +{code.reduce((partial_sum, a) => partial_sum + a, 0)})</span>*/}
+        {/*}*/}
+        { broken &&
+            <h3>Code broken in {history.length} attempts</h3>
+        }
+      </div>
     </div>
-    </div>;
+    <div className="keyboard">
+      { [0,1,2,3,4].map(key => <span onClick={() => handler({key})} key={key} className={`cell ${findBestHistoricalPlacement(key)}`}>{key}</span>) }
+      <br/>
+      { [5,6,7,8,9].map(key => <span onClick={() => handler({key})} key={key} className={`cell  ${findBestHistoricalPlacement(key)}`}>{key}</span>)}
+      <br/>
+      <span onClick={() => handler({key: 'Backspace'})} className={`cell`}>{'⌫'}</span>
+      <span onClick={() => handler({key: 'Enter'})} className={`cell`}>{'⏎'}</span>
+    </div>
+  </div>;
 }
